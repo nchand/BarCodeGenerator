5a836b24c3a4c91e0203f892ac17c7a602cd8496
Abdus <azad3652@gmail.com>
2018-05-08 16:18:45 +0530

    added 'price'

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,20 +9,25 @@ let wrapper1 = document.getElementById('cost');
 let hiddenBarcode = document.getElementById('hidden-barcode');
 let span = document.createElement('span');
 let img = document.createElement('img');
+let priceImgWrapper = document.createElement('div')
 img.setAttribute('class', 'barcode');
 span.setAttribute('class', 'barcodePrice');
+priceImgWrapper.setAttribute('class', 'priceImgWrapper');
 let barcodePrice = document.getElementsByClassName('barcodePrice');
 
 const generateBarcode = () => {
     wrapper.innerHTML = "";
     for (let i=0; i<productCount.value; i++) {
-        wrapper.appendChild(img)
-        wrapper1.appendChild(span);
+        priceImgWrapper.appendChild(span);
+        priceImgWrapper.appendChild(img)
         span = document.createElement('span'); 
+        wrapper.appendChild(priceImgWrapper);
         barcodePrice[i].innerHTML=productPrice.value; 
         img = document.createElement('img');
+        priceImgWrapper = document.createElement('div')
         img.setAttribute('class', 'barcode');
         span.setAttribute('class', 'barcodePrice');
+        priceImgWrapper.setAttribute('class', 'priceImgWrapper');
     } 
     JsBarcode(".barcode", productDataString.value);
     console.log(productDataString.value)
@@ -33,3 +38,7 @@ const generateBarcode = () => {
 submitbButton.addEventListener('click', () => {
     generateBarcode();
 })
+
+
+// basically what I did is, I created another div element 'priceImgWrapper' and append span and img to it. Than, finally I append 'priceImgWrapper' to original 'wrapper'
+// I positioned everything with CSS flex box. You can see those changes in CSS file.
